07319d85b171afda680175b89eea1b992bc33274
Leon Montealegre <leonm99@gmail.com>
2021-05-24 21:40:46 -0400

    Made Array.zip work for any amount of arrays

--- ts/utils/Array.ts
+++ ts/utils/Array.ts
@@ -4,16 +4,19 @@ declare global {
         with: <T2>(f: (v: T2, i: number) => T2) => Array<T2>;
     }
     interface ArrayConstructor {
-        zip: <T1, T2>(arr1: T1[], arr2: T2[]) => [T1, T2][];
+        zip: <T extends Array<any>>(...arrs: { [K in keyof T]: Array<T[K]> }) => T[];
     }
 }
+
 
 Array.prototype.with = function<T2>(f: (v: T2, i: number) => T2) {
     return new Array(this.length).fill(undefined).map(f);
 }
 
-Array.zip = function<T1, T2>(arr1: T1[], arr2: T2[]) {
-    return arr1.map((v1, i) => [v1, arr2[i]]);
+Array.zip = function<T extends Array<any>>(...arrs: { [K in keyof T]: Array<T[K]> }) {
+    return new Array(Math.min(...arrs.map(arr => arr.length)))
+        .fill(undefined)
+        .map((_, i) => arrs.map(arr => arr[i])) as T;
 }
 
 export {};
